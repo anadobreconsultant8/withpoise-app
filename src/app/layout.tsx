9e46cafdbf7eee0046abdd23b42cb38613b2f4ff
Ana Dobre <ana@withpoise.com>
2026-03-17 14:20:13 +0200

    Add Meta Pixel and update CSP for Facebook domains

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { SessionProvider } from '@/components/session-provider'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 
 const BASE_URL = process.env.NEXTAUTH_URL || 'https://withpoise.net'
 
@@ -55,6 +56,24 @@ export default function RootLayout({
       <body>
         <SessionProvider>{children}</SessionProvider>
         <Analytics />
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2376244492879795');
+          fbq('track', 'PageView');
+        `}</Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=2376244492879795&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   )
